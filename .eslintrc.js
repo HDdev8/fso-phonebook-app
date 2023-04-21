ea2f7201c26736178dcc478df389ef0bf03cb3a0
HDdev8 <pantonakos@my.waketech.edu>
2023-04-20 23:15:42 -0400

    uibuild

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+	env: {
+		node: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: ["eslint:recommended", "prettier"],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	plugins: ["react"],
+	rules: {
+		"linebreak-style": ["error", "unix"],
+	},
+};
--- info.js
+++
@@ -1,4 +0,0 @@
-exports.info = {
-  content: "",
-  date: "",
-};
